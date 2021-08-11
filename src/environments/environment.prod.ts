8fd8a98ef38340149a0b6d6f330899a1c5b2e370
ShannonCH <shannon26.c@gmail.com>
2021-08-11 11:01:29 -0400

    add oshop

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAOJMIU_9BQA9-a5IUZtqBFKVaYHGUuaMU",
+    authDomain: "oshop-736dc.firebaseapp.com",
+    projectId: "oshop-736dc",
+    storageBucket: "oshop-736dc.appspot.com",
+    messagingSenderId: "411414809978",
+    appId: "1:411414809978:web:beaa0c2257a10bffd49ac3"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAOJMIU_9BQA9-a5IUZtqBFKVaYHGUuaMU",
+    authDomain: "oshop-736dc.firebaseapp.com",
+    projectId: "oshop-736dc",
+    storageBucket: "oshop-736dc.appspot.com",
+    messagingSenderId: "411414809978",
+    appId: "1:411414809978:web:beaa0c2257a10bffd49ac3"
+  }
 };
 
 /*
